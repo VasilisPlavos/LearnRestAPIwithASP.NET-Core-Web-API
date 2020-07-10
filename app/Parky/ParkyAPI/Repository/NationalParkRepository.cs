0151cabfc0d589cbbac30b53159a1c017056d53c
vasilis <22474769+VasilisPlavos@users.noreply.github.com>
2020-07-10 12:54:25 +0300

    creata the methods in NationalParkRepo

--- app/Parky/ParkyAPI/Repository/NationalParkRepository.cs
+++ app/Parky/ParkyAPI/Repository/NationalParkRepository.cs
@@ -1,4 +1,6 @@
-﻿using ParkyAPI.Repository.IRepository;
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,58 @@ namespace ParkyAPI.Repository
 {
     public class NationalParkRepository : INationalParkRepository
     {
+       
+        /// I need these to connect with the database
+        private readonly ApplicationDbContext _db;
+        public NationalParkRepository(ApplicationDbContext db) { _db = db; }
+
+
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            NationalPark nationalPark = _db.NationalParks.Find(nationalParkId);
+            return _db.NationalParks.FirstOrDefault(nationalPark => nationalPark.Id == nationalParkId);
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            // this works also but we want them OrderByName
+            // return _db.NationalParks.ToList();
+            return _db.NationalParks.OrderBy(nationalPark => nationalPark.Name).ToList();
+
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            bool value = _db.NationalParks.Any(park => park.Name.ToLower().Trim() == name.ToLower().Trim() );
+            return value;
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NationalParks.Any(park => park.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Update(nationalPark);
+            return Save();
+        }
     }
 }
