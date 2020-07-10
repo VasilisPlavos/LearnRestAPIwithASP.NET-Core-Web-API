9a9dc2acb4fb1e784b1ceb1373fb0637d13d07ea
vasilis <22474769+VasilisPlavos@users.noreply.github.com>
2020-07-10 12:31:54 +0300

    create NationalParkRepo and it's Interface to create the methods
    in the INationalParkRepo I say that methods I need. After this, it will ask me to express them on NPRepo

---
+++ app/Parky/ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,12 @@
+﻿using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+    }
+}
