a89623a190d935e7ebfb6f69e0548fecefcee035
vasilis <22474769+VasilisPlavos@users.noreply.github.com>
2020-07-10 16:00:56 +0300

    Update NationalParkDto.cs

--- app/Parky/ParkyAPI/Models/Dtos/NationalParkDto.cs
+++ app/Parky/ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -11,8 +11,10 @@ namespace ParkyAPI.Models.Dtos
   
         public int Id { get; set; }
   
+        [Required]
         public string Name { get; set; }
      
+        [Required]
         public string State { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
